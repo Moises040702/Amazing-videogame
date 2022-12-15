562dce20aac12d1d58b1a23e98e0b0a3ce31f458
JorgeRodriguez <jrr150502@gmail.com>
2022-12-15 11:15:27 +0100

    Segundo commit

--- Escenario.java
+++ Escenario.java
@@ -10,4 +10,20 @@ public class Escenario
         tamaño = tamañoDelPersonaje;
         llueve = true;
     }
+
+    public float getTamaño() {
+        return tamaño;
+    }
+
+    public boolean getLlueve() {
+        return llueve;
+    }
+
+    public void setTamaño(float otroTamaño) {
+        tamaño = otroTamaño;
+    }
+
+    public void setLlueve(boolean otroLlueve) {
+        llueve = otroLlueve;
+    }
 }
