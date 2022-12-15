56267dd1426ade6a66669b8a281259597abc2173
Moisés <moises.celmar@educa.jcyl.es>
2022-12-15 11:08:03 +0100

    Primer commit

---
+++ Escenario.java
@@ -0,0 +1,13 @@
+public class Escenario
+{
+    private float tamaño;
+    private boolean llueve;
+    /**
+     * Constructor for objects of class Escenario
+     */
+    public Escenario(float tamañoDelPersonaje)
+    {
+        tamaño = tamañoDelPersonaje;
+        llueve = true;
+    }
+}
